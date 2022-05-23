da09cc4aa8ddb7a7252d6f4f43c9258536dd72b7
Gigalomaniac <hieuht@its-global.vn>
2022-05-23 17:57:59 +0700

    update_develop_fe

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ngManageWarehouse'`, () => {
+  it(`should have as title 'Warehouse Management'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ngManageWarehouse');
+    expect(app.title).toEqual('Warehouse Management');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ngManageWarehouse app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'Warehouse Management app is running!'
+    );
   });
 });
