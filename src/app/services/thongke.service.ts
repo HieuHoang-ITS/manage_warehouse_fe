0c251175a071d27fcc7323eceeddb1ba933443a0
sond13cnpm7htyt20 <sonh@its-global.vn>
2022-06-06 15:34:06 +0700

    .

--- src/app/services/thongke.service.ts
+++ src/app/services/thongke.service.ts
@@ -4,36 +4,51 @@ import { Observable, of } from 'rxjs';
 import { Product } from '../models/product';
 import { productTable, ThongKe } from '../models/thongke';
 import { pieChart } from '../models/thongke';
-import {doughChart} from '../models/thongke';
+import { doughChart } from '../models/thongke';
 // import { ThongKe } from '../models/Thongke';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ThongkeService {
-  thongkeUrl="http://localhost:8080/thang1"
-  pieChartUrl="http://localhost:8080/api/thongkeloaixuat/"
-  piechartUrlnhap ="http://localhost:8080/api/thongkeloainhap/"
-  doughnutChartxuat ="http://localhost:8080/api/thongkeloaixuat/"
-  doughnutChartnhap  = "http://localhost:8080/api/thongkeloainhap/"
-  productListURL = "http://localhost:8080/thongkebasanphamnhapnhieunhat/";
-  constructor(private httpclient: HttpClient) { }
-  getDoanhThuTheoThang() : Observable<ThongKe[]>
-  {
+  thongkeUrl = 'http://localhost:8080/thang1';
+  pieChartUrl = 'http://localhost:8080/api/thongkeloaixuat/';
+  piechartUrlnhap = 'http://localhost:8080/api/thongkeloainhap/';
+  doughnutChartxuat = 'http://localhost:8080/api/thongkeloaixuat/';
+  doughnutChartnhap = 'http://localhost:8080/api/thongkeloainhap/';
+  productListURL = 'http://localhost:8080/thongkebasanphamnhapnhieunhat/';
+  constructor(private httpclient: HttpClient) {}
+  getDoanhThuTheoThang(): Observable<ThongKe[]> {
     return this.httpclient.get<ThongKe[]>(this.thongkeUrl).pipe();
   }
+  getDoanhThuTheoKhoangThang(from: string, to: string) {
+    let url =
+      'http://localhost:8080/thongketong' + '?from=' + from + '&to=' + to;
+    console.log(url);
+    return this.httpclient.get<ThongKe[]>(url).pipe();
+  }
   getExportPerc(month: number, year: number) {
-    return this.httpclient.get<pieChart[]>(this.pieChartUrl+month+'/'+year)
+    return this.httpclient.get<pieChart[]>(
+      this.pieChartUrl + month + '/' + year
+    );
   }
   getImportPerc(month: number, year: number) {
-    return this.httpclient.get<pieChart[]>(this.piechartUrlnhap+month+'/'+year)
+    return this.httpclient.get<pieChart[]>(
+      this.piechartUrlnhap + month + '/' + year
+    );
   }
   getExportDouc(month: number, year: number) {
-    return this.httpclient.get<doughChart[]>(this.doughnutChartxuat+month+'/'+year)
+    return this.httpclient.get<doughChart[]>(
+      this.doughnutChartxuat + month + '/' + year
+    );
   }
   getImportDouc(month: number, year: number) {
-    return this.httpclient.get<doughChart[]>(this.doughnutChartnhap+month+'/'+year)
+    return this.httpclient.get<doughChart[]>(
+      this.doughnutChartnhap + month + '/' + year
+    );
   }
   getProductTable(month: number, year: number) {
-    return this.httpclient.get<productTable[]>(this.productListURL+month+ '/'+year)
+    return this.httpclient.get<productTable[]>(
+      this.productListURL + month + '/' + year
+    );
   }
 }
